6741be5134cad98d2ef4c87bbb86b56209703671
Valencua <valencuavasula@gmail.com>
2026-08-11 18:37:17 +0100

    banner + separador cronograma

--- web/routes/__init__.py
+++ web/routes/__init__.py
@@ -4,4 +4,3 @@ from web.routes.index import index_bp
 web_bp = Blueprint('web', __name__)
 
 web_bp.register_blueprint(index_bp)
-#otras pestañas abajo
--- web/routes/index.py
+++ web/routes/index.py
@@ -2,6 +2,10 @@ from flask import Blueprint, render_template
 
 index_bp = Blueprint('index', __name__)
 
+@index_bp.route('/cronograma')
+def cronograma():
+    return render_template('cronograma.html')
+
 @index_bp.route('/cursada')
 def cursada():
     return render_template('cursada.html')
