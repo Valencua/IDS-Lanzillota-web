e0ac8db2a46b3fba9ff401d3f20f141aebae3c86
Valencua <valencuavasula@gmail.com>
2026-08-11 03:36:09 +0100

    feat: template cursada

--- web/routes/index.py
+++ web/routes/index.py
@@ -2,6 +2,10 @@ from flask import Blueprint, render_template
 
 index_bp = Blueprint('index', __name__)
 
+@index_bp.route('/cursada')
+def cursada():
+    return render_template('cursada.html')
+
 @index_bp.route('/')
 def index():
     return render_template('inicio.html')
